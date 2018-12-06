9337b3dcb0bd22a0faf308cdc7e9ed048f40004b
nature-sky <vbnwerbnm@gmail.com>
2018-12-07 00:53:33 +0800

    Increase Bot version number.

--- bot.user.js
+++ bot.user.js
@@ -23,13 +23,14 @@ SOFTWARE.*/
 // ==UserScript==
 // @name        AposBot
 // @namespace   AposBot
-// @include     http://agar.io/*, http://agario.fun/*
-// @version     3.652
+// @include     http://agar.io/*
+// @include     http://agario.fun/*
+// @version     3.653
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
 // ==/UserScript==
 
-var aposBotVersion = 3.652;
+var aposBotVersion = 3.653;
 
 //TODO: Team mode
 //      Detect when people are merging
