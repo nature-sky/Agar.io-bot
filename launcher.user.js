c8c830e315d7363e983997e46316d59f3cb6efff
nature-sky <vbnwerbnm@gmail.com>
2018-12-06 23:49:13 +0800

    Modify Pb().

--- launcher.user.js
+++ launcher.user.js
@@ -260,7 +260,9 @@ getLatestCommit();
         e("#overlays").hide();
         e("#stats").hide();
         e("#mainPanel").hide();
-        aa = ja = !1;
+        //aa = ja = !1;
+        aa = !1; 
+        ja = !1;
         Qb();
         c.destroyAd(c.adSlots.aa);
         c.destroyAd(c.adSlots.ac)
