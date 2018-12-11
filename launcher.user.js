5c52bf0de202f574a9e0e9626252029e6164b339
nature-sky <vbnwerbnm@gmail.com>
2018-12-11 14:48:18 +0800

    Modify firstStart.

--- launcher.user.js
+++ launcher.user.js
@@ -642,12 +642,7 @@ var keyim = $(".mykey").val();
         //UPDATE
         if (firstStart) {
             //Uc(false);
-            ub = Date.now(), Ua = !1, fa || V || (vb ? (nb(d.ab), Yb(), V = !0, setTimeout(function() {
-                e("#overlays").fadeIn(500, function() {
-                    Y()
-                });
-                e("#stats").show();
-            }, 1500)) : pa(500))
+            ub = Date.now(), Ua = !1, fa || V || (vb ? (nb(d.ab), Yb(), V = !0, e("#stats").show() : pa(500))
         }
 
         if (getPlayer().length == 0 && !reviving && ~~(getCurrentScore() / 100) > 0) {
