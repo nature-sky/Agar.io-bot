2fb424a857c0d4cf936fec2281cc91d643d918d3
nature-sky <vbnwerbnm@gmail.com>
2018-12-12 14:50:31 +0800

    Modify Uc().

--- launcher.user.js
+++ launcher.user.js
@@ -164,12 +164,12 @@ var keyim = $(".mykey").val();
             c = !1;
         d.onkeydown = function(n) {
             //UPDATE
-	    if (!window.jQuery('#nick').is(":focus")) {
+            if (!window.jQuery('#nick').is(":focus")) {
 	        32 != n.keyCode || a || ("nick" != n.target.id && n.preventDefault(), ca(), H(17), a = !0);
 	        81 != n.keyCode || b || (H(18), b = !0);
 	        87 != n.keyCode || c || (ca(), H(21), c = !0);
 	        27 == n.keyCode && (n.preventDefault(), pa(300))
-		keyAction(n)
+                keyAction(n)
             }
         };
         d.onkeyup = function(n) {
@@ -641,8 +641,13 @@ var keyim = $(".mykey").val();
     function ca() {
         //UPDATE
         if (firstStart) {
-            //Uc(false);
-            (fa || V || (vb ? (nb(d.ab), Yb(), V = !0, e("#overlays").fadeIn(3E3), e("#stats").show()) : pa(3E3)))
+            //Uc(false)
+            fa || V || (vb ? (nb(d.ab), Yb(), V = !0, setTimeout(function() {
+                e("#overlays").fadeIn(500, function() {
+                    Y()
+                });
+                e("#stats").show())
+            }, 1500)) : pa(500))
         }
 
         if (getPlayer().length == 0 && !reviving && ~~(getCurrentScore() / 100) > 0) {
