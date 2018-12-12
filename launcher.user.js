3275dacce7f86c9a53f5f8b5485879b932e2f795
nature-sky <vbnwerbnm@gmail.com>
2018-12-12 16:36:04 +0800

    Modify Uc().

--- launcher.user.js
+++ launcher.user.js
@@ -1202,7 +1202,7 @@ var keyim = $(".mykey").val();
                     };
                 d.requestAnimationFrame(g)
             }
-        } //else e(".agario-profile-panel .progress-bar-star").text(a.d), e(".agario-exp-bar .progress-bar-text").text(a.e + "/" + a.c + " XP"), e(".agario-exp-bar .progress-bar").css("width", (88 * a.e / a.c).toFixed(2) + "%"), b && b()
+        } else e(".agario-profile-panel .progress-bar-star").text(a.d), e(".agario-exp-bar .progress-bar-text").text(a.e + "/" + a.c + " XP"), e(".agario-exp-bar .progress-bar").css("width", (88 * a.e / a.c).toFixed(2) + "%"), b && b()
     }
 
     function Fb(a) {
@@ -1363,7 +1363,7 @@ var keyim = $(".mykey").val();
     function Uc() {
 		fa || V || (vb ? (nb(d.ab), Yb(), V = !0, setTimeout(function() {
 			e("#overlays").fadeIn(500, function() {
-				Y()
+				//Y()
 			});
 			e("#stats").show()
 		}, 1500)) : pa(500))
