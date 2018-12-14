7eb4de2c8a5afa112203caed6c8e2b0553098571
nature-sky <vbnwerbnm@gmail.com>
2018-12-14 15:45:12 +0800

    Modify Y().

--- launcher.user.js
+++ launcher.user.js
@@ -1208,7 +1208,7 @@ wheel = gb;
                     };
                 d.requestAnimationFrame(g)
             }
-        } //else e(".agario-profile-panel .progress-bar-star").text(a.d), e(".agario-exp-bar .progress-bar-text").text(a.e + "/" + a.c + " XP"), e(".agario-exp-bar .progress-bar").css("width", (88 * a.e / a.c).toFixed(2) + "%"), b && b()
+        } else e(".agario-profile-panel .progress-bar-star").text(a.d), e(".agario-exp-bar .progress-bar-text").text(a.e + "/" + a.c + " XP"), e(".agario-exp-bar .progress-bar").css("width", (88 * a.e / a.c).toFixed(2) + "%"), b && b()
     }
 
     function Fb(a) {
